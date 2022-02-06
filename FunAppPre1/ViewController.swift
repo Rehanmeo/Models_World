7614461bd13fdebb5fa0bb9ff20dadbed1e611e7
Muhammad Rehan <mrehanmeo@gmail.com>
2022-02-07 01:58:57 +0500

    Initial Commit

---
+++ FunAppPre1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FunAppPre1
+//
+//  Created by Muhammad Rehan on 2/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
